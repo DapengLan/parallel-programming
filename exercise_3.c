3d1c55f92a59149639dc25b935512a2916f9b5c1
ParallelProg2015 <landapeng@gainestown.kbs.local>
2015-04-23 23:29:52 +0200

    exercise_3

--- exercise_3.c
+++ exercise_3.c
@@ -1,41 +1,40 @@
-// a game of Telephone
-// author : Dapeng Lan
-// Time	  : 22/4/2015
-
-#include<mpi.h>
-#include<stdio.h>
-#include<stdlib.h>
-
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-    
-    MPI_Init(NULL,NULL); // Initial the MPI environment
-    
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size); //Get the number of the processor
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  // Find out rank, size
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);  // Get the rank of the processor 
-
-    int token;
-    if (world_rank !=0) {
-	MPI_Recv(&token, 1, MPI_INT, (world_rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	printf(" Process %d received token %d from process %d\n", world_rank, token, world_rank -1);
-	}
-   else { 
-	token = -1;  // set the value token	
-	}
-MPI_Send(&token, 1 , MPI_INT, (world_rank + 1) % world_size, 0 , MPI_COMM_WORLD); // every processor is sending to the nex
-
-//process 0 can receice from the last process. 
-
-if (world_rank == 0 ){
-	MPI_Recv(&token, 1, MPI_INT, world_size -1 ,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	printf(" Process %d received token %d from process %d\n", world_rank, token, world_size -1);
+  char token[100];
+ strcpy(token, argv[1]);
+  // Receive from the lower process and send to the higher process. Take care
+  // of the special case when you are the first process to prevent deadlock.
+  if (world_rank != 0) {
+    MPI_Recv(&token, strlen(token), MPI_BYTE, world_rank - 1, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    printf("Process %d received token %s from process %d\n", world_rank, token,
+           world_rank - 1);
+  } else {
+    // Set the token's value if you are process 0
+    strcpy(token, argv[1]);
+  }
+  MPI_Send(&token, strlen(token), MPI_BYTE, (world_rank + 1) % world_size, 0,
+           MPI_COMM_WORLD);
+  // Now process 0 can receive from the last process. This makes sure that at
+  // least one MPI_Send is initialized before all MPI_Recvs (again, to prevent
+  // deadlock)
+  if (world_rank == 0) {
+    MPI_Recv(&token, strlen(token), MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    printf("Process %d received token %s from process %d\n", world_rank, token,
+           world_size - 1);
+  }
+  MPI_Finalize();
 }
-	MPI_Finalize();
-}
-
-
-
-
